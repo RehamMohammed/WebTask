9d200629151df73515ef3aee2c9ef11eb616cfac
RehamMohamed <rery.mohamed1100@gmail.com>
2021-10-17 23:54:50 +0200

    commit3

--- charts/views.py
+++ charts/views.py
@@ -46,6 +46,9 @@ def get_page2(request):
 def get_page3(request):
     return render(request, 'figure3.html')
 
+def get_page4(request):
+    return render(request, 'figure4.html')
+
 class ChartData(APIView):
     authentication_classes = []
     permission_classes = []
@@ -66,25 +69,7 @@ class ChartData(APIView):
                         "chartdata":chartdata,
                 }
         return Response(data)
-'''class ChartData2(APIView):
-    authentication_classes = []
-    permission_classes = []
-    def get(self,request , format = None):
-        dict = get_data('affiliate_provider')
-        d_descending = OrderedDict(sorted(dict.items(), key=lambda kv: kv[1], reverse=True))
-        keys = []
-        values = []
-        keys.append(d_descending.keys())
-        values.append(d_descending.values())
-        labels = keys[0]
-        chartLabel = "affiliate_provider"
-        chartdata = values[0]
-        data2 ={
-                        "labels":labels,
-                        "chartLabel":chartLabel,
-                        "chartdata":chartdata,
-                }
-        return Response(data2)'''
+
 def get_fig2(request):
         dict = get_data('affiliate_provider')
         d_descending = OrderedDict(sorted(dict.items(), key=lambda kv: kv[1], reverse=True))
@@ -122,20 +107,69 @@ def get_fig3(request):
         return Response(data)
 
 def get_fig4(request):
-        dict = get_data('country_destination')
-        sortednames=sorted(dict.keys(), key=lambda x:x.lower())
-        keys = []
-        values = []
-        for i in sortednames:
-           keys.append(i)
-           values.append(dict[i])
-        
-        labels = keys[0]
-        chartLabel = "country_destination"
-        chartdata = values[0]
-        data ={
-                        "labels":labels,
-                        "chartLabel":chartLabel,
-                        "chartdata":chartdata,
-                }
-        return Response(data)
+    dff = set(dataf["signup_app"].values)
+  
+    new_df = dataf[['signup_app','age']].dropna()
+    #print(new_df.groupby('signup_app')['age'].apply(list))
+    Web = []
+    Android = []
+    Moweb = []
+    Ios = []
+
+    Android = new_df.groupby('signup_app')['age'].apply(list)[0]
+    Moweb = new_df.groupby('signup_app')['age'].apply(list)[1]
+    Web = new_df.groupby('signup_app')['age'].apply(list)[2]
+    Ios = new_df.groupby('signup_app')['age'].apply(list)[3]
+    def no_of_users(Web):
+        l1 = []
+        range1 = 0
+        range2 = 0
+        range3 = 0
+        range4 = 0
+        range5 = 0
+        range6 = 0
+        range7 = 0
+        for i in Web:
+            if i >= 18 and i< 20:
+                range1 += 1
+            elif i >= 20 and i <30:
+                range2 += 1
+            elif i >= 30 and i <40:
+                range3 += 1
+            elif i >= 40 and i <50:
+                range4 += 1
+            elif i >= 50 and i <60:
+                range5 += 1
+            elif i >= 60 and i <70:
+                range6 += 1
+            elif i >= 70: 
+                range7 += 1
+        l1.append(range1)
+        l1.append(range2)
+        l1.append(range3)
+        l1.append(range4)
+        l1.append(range5)
+        l1.append(range6)
+        l1.append(range7)
+        return l1
+
+    dict2 = {}
+
+    dict2['Android'] = no_of_users(Android)
+    dict2['Moweb'] = no_of_users(Moweb)
+    dict2['Web'] = no_of_users(Web)
+    dict2['iOS'] = no_of_users(Ios)
+    keys2 = []
+    keys2.append(dict2.keys())
+    values2 = []
+    values2.append(dict2.values())
+    
+    labels = keys2[0]
+    chartLabel = "signup app"
+    chartdata = values2[0]
+    data ={
+            "labels":labels,
+            "chartLabel":chartLabel,
+            "chartdata":chartdata,
+        }
+    return Response(data)
--- interviewtask/urls.py
+++ interviewtask/urls.py
@@ -19,12 +19,14 @@ from charts import  views
 
 urlpatterns = [
     path('', views.get_page1),
-   
+
     path('fig2/', views.get_page2),
     path('fig3/', views.get_page3),
+    path('fig4/', views.get_page4),
     path('api' , views.ChartData.as_view()),
-    path('api2' , views.get_fig2),
-    path('api3' , views.get_fig3),
-    #path('api2' , views.ChartData2.as_view() ),
+    path('fig2' , views.get_fig2), 
+    path('fig3' , views.get_fig3),
+    path('fig4' , views.get_fig4),
+    #path('api2' , views.ChartData2.as_view()),
     
 ]
